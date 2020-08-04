573ef1eedf77a57b84915bf7f7dfed80052d5e0c
unknown <g.hitoshi.yokogawa@gmail.com>
2020-08-04 11:51:00 -0300

    dml da optus feita

--- Optus/DML/Optus.sql
+++ Optus/DML/Optus.sql
@@ -1,1 +1,12 @@
 USE optus;
+
+INSERT INTO artista (Nome) VALUES('Frank Ocean');
+
+INSERT INTO album (Nome, DataLancamento, Localizacao, QtdMinutos, IdArtista) 
+			VALUES('Blonded', '2016-08-16', 'usa', 60, 1);
+
+INSERT INTO estilo (Estilo) VALUES ('hip-hop');
+
+SELECT * FROM artista;
+SELECT * FROM album;
+SELECT * FROM estilo;
