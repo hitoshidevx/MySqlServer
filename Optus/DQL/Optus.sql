a1326a305c1ea5ceba95eb52ce2bd8591e413a75
hitoshi <g.hitoshi.yokogawa@gmail.com>
2020-08-05 10:35:34 -0300

    DQL da optus finalizada.
    Estudo simples de DQL feito com a optus.

--- Optus/DQL/Optus.sql
+++ Optus/DQL/Optus.sql
@@ -1,3 +1,17 @@
 USE optus;
 
 SELECT * FROM album;
+
+SELECT * FROM album WHERE IdArtista = 2;
+
+SELECT * FROM album WHERE QtdMinutos > 40;
+
+SELECT * FROM album WHERE Nome LIKE '%ded';
+
+SELECT * FROM album ORDER BY Nome;
+
+SELECT * FROM album ORDER BY Nome DESC;
+
+SELECT * from album ORDER BY DataLancamento;
+
+SELECT * FROM album ORDER BY DataLancamento DESC;
