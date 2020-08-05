998f9c6a1d466f30ebfe27f75f1f87772345d8d1
hitoshi <g.hitoshi.yokogawa@gmail.com>
2020-08-05 10:11:31 -0300

    teste de dql iniciado.
    teste com o intuito de explorar algumas das funcionalidades da dql.

--- ClinicaVet/DQL/ClinicaVet.sql
+++ ClinicaVet/DQL/ClinicaVet.sql
@@ -3,3 +3,11 @@ USE clinica;
 SELECT * FROM pet;
 
 SELECT * FROM pet WHERE IdRaca = 3;
+
+SELECT * FROM pet WHERE Nome LIKE '%o%';
+
+SELECT * FROM pet WHERE IdDono > 2;
+
+SELECT * FROM dono;
+
+SELECT * FROM dono WHERE Nome LIKE '%o';
