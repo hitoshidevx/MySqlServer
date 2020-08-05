05842bc85b63d635c105d6aeee8f02c903fe6147
hitoshi <g.hitoshi.yokogawa@gmail.com>
2020-08-05 10:20:29 -0300

    DQL feita.
    estudo simples de consulta terminado.

--- ClinicaVet/DQL/ClinicaVet.sql
+++ ClinicaVet/DQL/ClinicaVet.sql
@@ -1,13 +1,19 @@
 USE clinica;
 
 SELECT * FROM pet;
+/*DQL - Data Query Language (SELECT)*/
 
+--Seleção de um dado específico.
 SELECT * FROM pet WHERE IdRaca = 3;
 
+--Seleção de dados filtrados pelo LIKE.
 SELECT * FROM pet WHERE Nome LIKE '%o%';
 
+--Seleção de dados onde o dado é filtrado por ser maior ou menor.
 SELECT * FROM pet WHERE IdDono > 2;
 
-SELECT * FROM dono;
+--Seleção de dados ordenados por ordem CRESCENTE.
+SELECT * FROM pet ORDER BY Nome;
 
-SELECT * FROM dono WHERE Nome LIKE '%o';
+--Seleção de dados ordenados por ordem DECRESCENTE.
+SELECT * FROM pet ORDER BY Nome DESC;
