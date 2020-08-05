800ea5703682e9ff9a6a7c9201dbe84746fc3b7a
hitoshi <61585229+hitoshidevx@users.noreply.github.com>
2020-08-04 22:49:06 -0300

    teste
    teste

--- Optus/DML/Optus.sql
+++ Optus/DML/Optus.sql
@@ -1,6 +1,6 @@
 USE optus;
 
-INSERT INTO artista (Nome) VALUES('Playboi Carti');
+INSERT INTO artista (Nome) VALUES('Doja Cat');
 
 INSERT INTO album (Nome, DataLancamento, Localizacao, QtdMinutos, IdArtista) 
 			VALUES('Die Lit', '2018-05-11', 'usa', 57.39, 2);
@@ -10,3 +10,5 @@ INSERT INTO estilo (Estilo) VALUES ('trap, hip-hop, rap');
 SELECT * FROM artista;
 SELECT * FROM album;
 SELECT * FROM estilo;
+
+DELETE FROM artista WHERE IdArtista = 3;
