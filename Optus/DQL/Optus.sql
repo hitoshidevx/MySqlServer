8f0b3ab417319aa3872ee631168080209c7285ac
hitoshi <g.hitoshi.yokogawa@gmail.com>
2020-08-06 10:02:30 -0300

    DQL joins - Optus
    DQL realizada na Optus.

--- Optus/DQL/Optus.sql
+++ Optus/DQL/Optus.sql
@@ -1,5 +1,5 @@
 USE optus;
-
+/*DQL - Data Query Language (SELECT)*/
 SELECT * FROM album;
 
 SELECT * FROM album WHERE IdArtista = 2;
@@ -15,3 +15,36 @@ SELECT * FROM album ORDER BY Nome DESC;
 SELECT * from album ORDER BY DataLancamento;
 
 SELECT * FROM album ORDER BY DataLancamento DESC;
+
+/*DQL - Join*/
+-- INNER JOIN
+SELECT 
+	album.Nome,
+	artista.Nome
+FROM artista
+	INNER JOIN album ON artista.IdArtista = album.IdAlbum
+;
+
+-- LEFT JOIN
+SELECT 
+	album.Nome,
+	artista.Nome
+FROM artista
+	LEFT JOIN album ON artista.IdArtista = album.IdAlbum
+;
+
+-- RIGHT JOIN
+SELECT 
+	album.Nome,
+	artista.Nome
+FROM artista
+	RIGHT JOIN album ON artista.IdArtista = album.IdAlbum
+;
+
+-- FULL OUTER JOIN
+SELECT 
+	album.Nome,
+	artista.Nome
+FROM artista
+	FULL OUTER JOIN album ON artista.IdArtista = album.IdAlbum
+;
